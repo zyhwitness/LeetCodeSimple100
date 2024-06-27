b3a854218ebfee7d816014abc10da35ce7e8582b
zyhwitness <zyhwitness23@gmail.com>
2024-06-27 22:36:41 +0800

    zyhwitness, feat: 回文数

--- src/simple100/LeetCode9.java
+++ src/simple100/LeetCode9.java
@@ -12,12 +12,12 @@ public class LeetCode9 {
 
     @Test
     public void test() {
-        int x = 1221;
-        System.out.println(isPalindrome(x));
+        int x = 1001;
+        System.out.println(isPalindrome2(x));
     }
 
     /**
-     *方法一：反转后进行比较
+     * 方法一：反转后进行比较
      */
     public boolean isPalindrome(int x) {
         //反转该数
@@ -26,7 +26,7 @@ public class LeetCode9 {
         for (int i = content.length() - 1; i >= 0; i--) {
             convert += content.charAt(i);
         }
-        if(convert.equals(content)){
+        if (convert.equals(content)) {
             return true;
         }
         return false;
@@ -36,8 +36,21 @@ public class LeetCode9 {
      * 方法二：双指针
      */
     public boolean isPalindrome2(int x) {
+        //判断是否为负数
+        if (x < 0) {
+            return false;
+        }
 
-        return false;
+        //计算比较次数
+        String content = String.valueOf(x);
+        int count = content.length() / 2;
+        for (int i = 0; i < count; i++) {
+            char c = content.charAt(i);
+            char c1 = content.charAt(content.length() - 1 - i);
+            if (c != c1) {
+                return false;
+            }
+        }
+        return true;
     }
-
 }
