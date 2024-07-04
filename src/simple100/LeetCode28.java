1acc3f23b488a87a6734aa8d8442ea71e7acb6e2
zyhwitness <zyhwitness23@gmail.com>
2024-07-04 18:22:08 +0800

    zyhwitness, feat: 找出字符串中第一个匹配项的下标

--- src/simple100/LeetCode28.java
+++ src/simple100/LeetCode28.java
@@ -12,8 +12,8 @@ public class LeetCode28 {
 
     @Test
     public void test() {
-        String haystack = "saabutsad";
-        String needle = "buta";
+        String haystack = "abcdefg";
+        String needle = "def";
         System.out.println(strStr(haystack, needle));
     }
 
@@ -21,11 +21,13 @@ public class LeetCode28 {
         int index = -1;
         String context = "";
         for (int i = 0; i < haystack.length(); i++) {
-            if(i + needle.length() > haystack.length()){
+            //注意 substring(i, i + needle.length()) 不能超过字符串长度
+            if (i + needle.length() > haystack.length()) {
                 break;
             }
+            //截取跟 needle 一样长度的字符串进行比较
             context = haystack.substring(i, i + needle.length());
-            if(context.equals(needle)){
+            if (context.equals(needle)) {
                 index = i;
                 break;
             }
