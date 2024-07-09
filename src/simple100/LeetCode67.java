1f920c15c9a8469ad611456cb61b5dd75c7b54f4
zyhwitness <zyhwitness23@gmail.com>
2024-07-09 23:53:20 +0800

    zyhwitness, feat: 二进制求和

--- src/simple100/LeetCode67.java
+++ src/simple100/LeetCode67.java
@@ -12,13 +12,13 @@ public class LeetCode67 {
 
     @Test
     public void test() {
-        String a = "1010";
+        String a = "1011";
         String b = "1101";
         System.out.println(addBinary(a, b));
     }
 
     public String addBinary(String a, String b) {
-        //先用0补齐位数，使得两个字符串长度一致
+        //先用 0 补齐位数，使得两个字符串长度一致
         if (a.length() >= b.length()) {
             int diff = a.length() - b.length();
             for (int i = 0; i < diff; i++) {
@@ -38,18 +38,20 @@ public class LeetCode67 {
         for (int i = a.length() - 1; i >= 0; i--) {
             int aValue = Integer.parseInt(a.charAt(i) + "");
             int bValue = Integer.parseInt(b.charAt(i) + "");
+            //计算每一位之和
             int value = aValue + bValue + jNum;
-            //判断是否进位
+            //判断是否进位，进位的话将进位值改为 1，下一个循环计算 value 时带上
             jNum = (value >= 2) ? 1 : 0;
+            //求和后该位数字（0，1，2，3 四种情况，求和为 0 或 2 则该位数字为 0）
             sum[i] = (value == 2 || value == 0) ? 0 : 1;
         }
 
+        //若循环结束后进位值为 1，则说明首位相加再加前一位进位值 >= 2，此时还需要向前进一位，因此补一位 1
         String strSum = (jNum == 1) ? "1" : "";
         for (int j : sum) {
             strSum += j;
         }
 
         return strSum;
-
     }
 }
