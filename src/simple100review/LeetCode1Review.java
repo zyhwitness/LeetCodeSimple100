5286ab5b03f55e0700c2eac8fce443050dea8c48
zyh-macbookpro <zyhwitness23@gmail.com>
2024-07-20 00:01:26 +0800

    zyhwitness, feat: 两数之和-review

--- src/simple100review/LeetCode1Review.java
+++ src/simple100review/LeetCode1Review.java
@@ -22,8 +22,8 @@ public class LeetCode1Review {
     public int[] twoSum(int[] nums, int target) {
         for (int i = 0; i < nums.length; i++) {
             for (int j = i + 1; j < nums.length; j++) {
-                int sum  = nums[i]  + nums[j];
-                if(sum == target){
+                int sum = nums[i] + nums[j];
+                if (sum == target) {
                     return new int[]{i, j};
                 }
             }
