64c41a27ce64cc04e703c4365e499a95efbdcd2b
zyh-macbookpro <zyhwitness23@gmail.com>
2025-03-24 14:37:42 +0800

    zyhwitness, feat: 删除有序数组中的重复项-review3

---
+++ src/simple100review3/LeetCode26Review3.java
@@ -0,0 +1,37 @@
+package simple100review3;
+
+import org.junit.Test;
+
+/**
+ * @Description: 删除有序数组中的重复项-review3
+ * @Author: iWitness
+ * @Date: 2025/3/24 14:18
+ * @Version 1.0
+ */
+public class LeetCode26Review3 {
+
+//    给你一个 非严格递增排列 的数组 nums ，请你 原地 删除重复出现的元素，使每个元素 只出现一次 ，返回删除后数组的新长度。元素的 相对顺序 应该保持 一致 。然后返回 nums 中唯一元素的个数。
+//
+//    考虑 nums 的唯一元素的数量为 k ，你需要做以下事情确保你的题解可以被通过：
+//
+//    更改数组 nums ，使 nums 的前 k 个元素包含唯一元素，并按照它们最初在 nums 中出现的顺序排列。nums 的其余元素与 nums 的大小不重要。
+//    返回 k 。
+
+    @Test
+    public void test() {
+        int[] nums = {1, 1, 1, 2, 2, 3, 4, 5, 5};
+        System.out.println(removeDuplicates(nums));
+    }
+
+    public int removeDuplicates(int[] nums) {
+        int index = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[index] != nums[i]) {
+                nums[++index] = nums[i];
+            }
+        }
+        return ++index;
+    }
+
+    // 设置一个初始索引 0，遍历数组，寻找不等于索引位置的值，索引后移，并将改值赋给索引位。
+}
