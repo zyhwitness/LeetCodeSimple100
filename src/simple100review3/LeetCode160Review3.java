a03d57a46043fbdecf2659ed6e32cda23fb6b1d7
zyh-macbookpro <zyhwitness23@gmail.com>
2025-04-28 20:06:53 +0800

    zyhwitness, feat: 相交链表 -review3

---
+++ src/simple100review3/LeetCode160Review3.java
@@ -0,0 +1,67 @@
+package simple100review3;
+
+import org.junit.Test;
+
+/**
+ * @Description: 相交链表 -review3
+ * @Author: iWitness
+ * @Date: 2025/4/28 19:54
+ * @Version 1.0
+ */
+public class LeetCode160Review3 {
+
+    @Test
+    public void test() {
+        ListNode headA = new ListNode(1);
+        ListNode headB = new ListNode(2);
+
+        ListNode list3 = new ListNode(3);
+        ListNode list4 = new ListNode(4);
+
+        headA.next = list3;
+        headB.next = list4;
+
+        ListNode list5 = new ListNode(5);
+
+        list3.next = list5;
+        list4.next = list5;
+
+
+        System.out.println(getIntersectionNode(headA, headB));
+
+    }
+
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+
+        while (headA != null) {
+            ListNode headBCopy = headB;
+            while (headBCopy != null) {
+                if (headA == headBCopy) {
+                    return headA;
+                }
+                headBCopy = headBCopy.next;
+            }
+            headA = headA.next;
+        }
+
+        return null;
+
+    }
+
+    public ListNode getIntersectionNode2(ListNode headA, ListNode headB) {
+        return null;
+    }
+
+    /**
+     * Definition for singly-linked list.
+     */
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+}
