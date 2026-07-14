30d2e610430a2131aecc389c77d9f782b9412554
zyhwitness <zyhwitness23@gmail.com>
2026-07-14 10:33:21 +0800

    feat: hot100-盛最多水的容器

---
+++ src/hot100/LeetCode11.java
@@ -0,0 +1,45 @@
+package hot100;
+
+import org.junit.Test;
+
+/**
+ * @Description: 盛最多水的容器
+ * @Author: iWitness
+ * @Date: 2026/7/14 9:49
+ * @Version 1.0
+ */
+public class LeetCode11 {
+
+//    给定一个长度为 n 的整数数组 height 。有 n 条垂线，第 i 条线的两个端点是 (i, 0) 和 (i, height[i]) 。
+//
+//    找出其中的两条线，使得它们与 x 轴共同构成的容器可以容纳最多的水。
+//
+//    返回容器可以储存的最大水量。
+//
+//    说明：你不能倾斜容器。
+
+    @Test
+    public void test() {
+        int[] height = {1, 8, 6, 2, 5, 4, 8, 3, 7};
+        System.out.println(maxArea(height));
+    }
+
+    public int maxArea(int[] height) {
+
+        int max = 0;
+        int i = 0, j = height.length - 1;
+        while (i < j) {
+            int size = (j - i) * Math.min(height[i], height[j]);
+            max = Math.max(max, size);
+            if (height[i] < height[j]) {
+                i++;
+            } else {
+                j--;
+            }
+        }
+
+        return max;
+    }
+
+    //
+}
